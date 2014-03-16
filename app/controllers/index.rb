88a5ac0d5edfeeca0547726d15468cc7ef95daa7
Ryan Leahy <ryanleahy90@gmail.com>
2014-03-15 21:44:13 -0500

    removed twitter

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,6 @@
 require 'sinatra'
-require 'twitter'
 
 get '/' do
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "grEjXegppSVVpNxeM8grDA"
-    config.consumer_secret     = "zIIajxPWLW1gbWVPv2jfCNZXX9RKaDmh0iKSwN83F94"
-    config.access_token        = "1109574216-jsib3NEouhqcORJpzpNGV7saUqL0NlFrH5AHtlj"
-    config.access_token_secret = "Kmd87VbzFnxMJskkKzvGT8ERy2zGmKPyPUcaadu2UGiuM"
-  end
-  @client = client.user_timeline('kristineleahy')
 	erb :index
 end
 
